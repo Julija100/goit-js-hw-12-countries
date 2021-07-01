bab15164795174ed43e5043e70d4cf16af343e7a
Julija Faltina <dashinicha@gmail.com>
2021-07-01 21:21:03 +0300

    Add coutries card

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,3 @@
-// fetchCountries(searchQuery);
 
-//Страны с https://restcountries.eu/#api-endpoints-name
 
-fetch('https://restcountries.eu/#api-endpoints-name')
-  //   .then(r => r.json())
-  .then(name => {
-    console.log(name);
-  })
-  .catch(error => console.log(error));
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './sass/main.scss';
+
+const getCountries = fetch('https://restcountries.eu/rest/v2/all')
+  .then(response => {
+    return response.json();
+  })
+  .then(country => {
+    console.log(country);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+// function fetchCountries(searchQuery) {
+//   fetch('https://restcountries.eu/rest/v2/all')
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(country => {
+//       console.log(country);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// }
