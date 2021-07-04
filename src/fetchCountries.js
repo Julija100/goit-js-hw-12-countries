55d12b9ecfcec24eb6718e810e33d190cf6c322e
Julija Faltina <dashinicha@gmail.com>
2021-07-04 11:40:24 +0300

    initial commit

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,19 @@
+import countryCard from './country_card.hbs';
 
+const refs = {
+  cardContainer: document.querySelector('.'),
+};
 
+fetchCountries('latvia')
+  .then(renderCountryCard)
+  .catch(error => console.log(error));
+function fetchCountries(name) {
+  return fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(response => {
+    return response.json();
+  });
+}
 
+function renderCountryCard(country) {
+  const markup = countryCard(country);
+  refs.cardContainer.innerHTML = markup;
+}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
 import './sass/main.scss';
+// import countryCard from './country_card.hbs';
 
-const getCountries = fetch('https://restcountries.eu/rest/v2/all')
-  .then(response => {
-    return response.json();
-  })
-  .then(country => {
-    console.log(country);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
-// function fetchCountries(searchQuery) {
-//   fetch('https://restcountries.eu/rest/v2/all')
-//     .then(response => {
-//       return response.json();
-//     })
-//     .then(country => {
-//       console.log(country);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// }
+// fetch('https://restcountries.eu/rest/v2/name/eesti')
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(country => {
+//     console.log(country);
+//     const markup = countryCard(country);
+//     console.log(markup);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
